32bb6bd32a3f685ce3a19eaf2092a712adbd5a7d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-09 10:17:11 +0000

    Deploy huangruitian/easy-ui to github.com/huangruitian/easy-ui.git:gh-pages

---
+++ precache-manifest.18b84dee9da92200d80ee1974ccc2583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1905ca4da7382428776466a4b72d84",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.fd74535cc5c097492e32.bundle.js"
+  },
+  {
+    "url": "runtime~main.fd74535cc5c097492e32.bundle.js"
+  },
+  {
+    "revision": "af232ef2deba47577f11",
+    "url": "static/css/main.3513b310.chunk.css"
+  },
+  {
+    "revision": "d33dcb6e01bb3044f9d8",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.fd74535cc5c097492e32.bundle.js"
+  },
+  {
+    "url": "vendors~main.fd74535cc5c097492e32.bundle.js.LICENSE.txt"
+  }
+]);
